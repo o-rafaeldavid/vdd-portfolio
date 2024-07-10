e4ad6c86866cd0fe3678f0dd6dc2384449cd9449
Rafael David <rafaelcapitaodavid@gmail.com>
2024-07-10 20:25:05 +0100

    responsividade blur background

--- app/components/global/blurBackground/index.tsx
+++ app/components/global/blurBackground/index.tsx
@@ -1,32 +1,37 @@
 import AbstractToBlur from '@/app/assets/global/abstractToBlur'
 import blurBackgroundStyle from './.module.scss'
+import { useWindowSize } from '@uidotdev/usehooks'
 
 const BlurBackground = () => {
+    const { width } = useWindowSize()
+    const mediumDesktop = () => width! > 1400
+    const biggerThanTablet = () => width! > 1024
     return (
         <div id={blurBackgroundStyle.blurDiv}>
             <AbstractToBlur
-                blur="8rem"
-                opacity={0.25}
+                blur='8rem'
+                opacity={0.35}
                 fill="rgb(0, 255, 194)"
                 top={'-5rem'}
-                left={'-45rem'}
+                left={mediumDesktop() ? '-45rem' : '-40rem'}
                 rotate='45deg'
+                scale={mediumDesktop() ? 1 : 0.7}
             />
             <AbstractToBlur
                 blur="16rem"
                 opacity={0.4}
                 fill="rgb(0, 178, 255)"
                 top={'-5rem'}
-                left={'-17rem'}
+                left={mediumDesktop() ? '-17rem' : '-23rem'}
             />
             <AbstractToBlur
                 blur="8rem"
-                opacity={0.2}
+                opacity={mediumDesktop() ? 0.2 : 0.1}
                 fill="rgb(0, 178, 255)"
                 top={'-20rem'}
-                right={'0rem'}
+                right={mediumDesktop() ? '0rem' : '-10rem'}
                 rotate='100deg'
-                scale={1.7}
+                scale={mediumDesktop() ? 1.7 : 1}
             />
             <AbstractToBlur
                 blur="8rem"
@@ -38,11 +43,19 @@ const BlurBackground = () => {
             />
             <AbstractToBlur
                 blur="20rem"
-                opacity={0.25}
+                opacity={mediumDesktop() ? 0.25 : 0.1}
                 fill="rgb(0, 178, 255)"
                 bottom={'-30rem'}
                 right={'10rem'}
                 rotate='210deg'
+            />
+            <AbstractToBlur
+                blur="10rem"
+                opacity={mediumDesktop() ? 1 : 0.1}
+                fill="rgb(16, 16, 16)"
+                bottom={'20%'}
+                right={'25%'}
+                rotate='230deg'
             />
         </div>
     )
