9f3d6b855e1c4217ab7d1aff941b103c863b1aa3
Rafael David <rafaelcapitaodavid@gmail.com>
2024-07-13 10:44:45 +0100

    responsividade fundo

--- app/assets/global/abstractToBlur.tsx
+++ app/assets/global/abstractToBlur.tsx
@@ -1,13 +1,20 @@
+import { mapear } from "@/app/utils/common/mapear"
+import { useAnimationFrame } from "framer-motion"
+import { useState } from "react"
+
 type AbstractToBlurType = {
     blur: string,
-    fill: string,
+    fill: 'azul-eletrico' | 'verde-agua' | 'background',
     opacity: number,
     top?: number | string,
     left?: number | string,
     right?: number | string,
     bottom?: number | string,
     rotate?: string,
-    scale?: number
+    scale?: number,
+    cicleTime?: number,
+    clockWise?: boolean,
+    radius: number
 }
 
 const AbstractToBlur = (
@@ -21,7 +28,17 @@ const AbstractToBlur = (
         bottom = 'unset',
         rotate,
         scale,
+        cicleTime = 10000,
+        clockWise = false,
+        radius
     }: AbstractToBlurType) => {
+
+    const [angle, setAngle] = useState<number>(0)
+    useAnimationFrame((time) => {
+        const nowTime = time % cicleTime
+        setAngle(a => mapear(time, 0, cicleTime, 0, ((clockWise) ? 1 : -1) * Math.PI * 2))
+    })
+
     return (
         <svg
             width="721"
@@ -38,12 +55,13 @@ const AbstractToBlur = (
                 right: right,
                 bottom: bottom,
                 rotate: rotate,
-                scale: scale
+                scale: scale,
+                translate: `${radius * Math.cos(angle)}rem ${radius * Math.sin(angle)}rem`
             }}
         >
             <path
                 d="M301.715 429.5C324.115 239.5 590.381 165.333 720.715 152C669.548 109.167 562.215 21.7 542.215 14.5C517.215 5.5 17.2146 0 3.7146 0C-7.0854 0 8.2146 154.333 17.2146 231.5L3.7146 934C203.381 899 577.015 824 474.215 804C345.715 779 273.715 667 301.715 429.5Z"
-                fill={fill}
+                fill={fill === 'azul-eletrico' ? "rgb(0, 178, 255)" : fill === 'verde-agua' ? "rgb(0, 255, 194)" : "rgb(16, 16, 16)"}
             />
         </svg>
     )
--- app/components/global/blurBackground/index.tsx
+++ app/components/global/blurBackground/index.tsx
@@ -1,61 +1,84 @@
+import { useWindowSize } from '@uidotdev/usehooks'
 import AbstractToBlur from '@/app/assets/global/abstractToBlur'
 import blurBackgroundStyle from './.module.scss'
-import { useWindowSize } from '@uidotdev/usehooks'
 
 const BlurBackground = () => {
-    const { width } = useWindowSize()
+    const { width, height } = useWindowSize()
     const mediumDesktop = () => width! > 1400
     const biggerThanTablet = () => width! > 1024
+    const biggerThanMobile = () => width! > 768
+    const mediumHeight = () => height! > 800
     return (
         <div id={blurBackgroundStyle.blurDiv}>
+            {/* Azul Cima Esquerda */}
             <AbstractToBlur
-                blur='8rem'
-                opacity={0.35}
-                fill="rgb(0, 255, 194)"
-                top={'-5rem'}
-                left={mediumDesktop() ? '-45rem' : '-40rem'}
-                rotate='45deg'
-                scale={mediumDesktop() ? 1 : 0.7}
+                blur={biggerThanTablet() ? '16rem' : (biggerThanMobile() && mediumHeight()) ? '10rem' : '14rem'}
+                opacity={biggerThanTablet() ? 0.4 : (biggerThanMobile() && mediumHeight()) ? 0.2 : 0.4}
+                fill='azul-eletrico'
+                top={biggerThanTablet() ? '-5rem' : (biggerThanMobile() && mediumHeight()) ? '-10rem' : '0rem'}
+                left={mediumDesktop() ? '-17rem' : biggerThanTablet() ? '-23rem' : (biggerThanMobile() && mediumHeight()) ? '-15rem' : '-17rem'}
+                rotate={biggerThanTablet() ? '0deg' : (biggerThanMobile() && mediumHeight()) ? '5deg' : '13deg'}
+                scale={(biggerThanMobile() && mediumHeight()) ? 1 : 1.3}
+                radius={biggerThanTablet() ? 7 : (biggerThanMobile() && mediumHeight()) ? 3 : 1.5}
+                cicleTime={13000}
             />
+            {/* Verde Cima Esquerda */}
             <AbstractToBlur
-                blur="16rem"
-                opacity={0.4}
-                fill="rgb(0, 178, 255)"
-                top={'-5rem'}
-                left={mediumDesktop() ? '-17rem' : '-23rem'}
+                blur={biggerThanTablet() ? '8rem' : (biggerThanMobile() && mediumHeight()) ? '6rem' : '10rem'}
+                opacity={biggerThanTablet() ? 0.35 : (biggerThanMobile() && mediumHeight()) ? 0.2 : 0.4}
+                fill='verde-agua'
+                top={biggerThanTablet() ? '-5rem' : '-30rem'}
+                left={mediumDesktop() ? '-45rem' : biggerThanTablet() ? '-40rem' : '-10rem'}
+                rotate={biggerThanTablet() ? '45deg' : '55deg'}
+                scale={mediumDesktop() ? 1 : biggerThanTablet() ? 0.7 : 0.8}
+                radius={biggerThanTablet() ? 3 : 1}
             />
-            <AbstractToBlur
+            {/* Azul Cima Direita */}
+            {biggerThanTablet() && <AbstractToBlur
                 blur="8rem"
                 opacity={mediumDesktop() ? 0.2 : 0.1}
-                fill="rgb(0, 178, 255)"
+                fill='azul-eletrico'
                 top={'-20rem'}
                 right={mediumDesktop() ? '0rem' : '-10rem'}
                 rotate='100deg'
                 scale={mediumDesktop() ? 1.7 : 1}
-            />
+                radius={2}
+                cicleTime={5000}
+                clockWise
+            />}
+            {/* Verde Meio Direita */}
+            {(biggerThanMobile() && mediumHeight()) && <AbstractToBlur
+                blur={biggerThanTablet() ? '8rem' : '4rem'}
+                opacity={biggerThanTablet() ? 0.25 : 0.15}
+                fill='verde-agua'
+                bottom={biggerThanTablet() ? '5rem' : '-12rem'}
+                right={biggerThanTablet() ? '-45rem' : '-25rem'}
+                rotate={biggerThanTablet() ? '230deg' : '200deg'}
+                scale={1}
+                radius={biggerThanTablet() ? 3 : 1.5}
+            />}
+            {/* Azul Baixo Direita */}
+            {(biggerThanMobile() && mediumHeight()) && <AbstractToBlur
+                blur={biggerThanTablet() ? '20rem' : '8rem'}
+                opacity={mediumDesktop() ? 0.25 : biggerThanTablet() ? 0.2 : 0.1}
+                fill='azul-eletrico'
+                bottom={biggerThanTablet() ? '-30rem' : '-25rem'}
+                right={biggerThanTablet() ? '10rem' : '-10rem'}
+                rotate={biggerThanTablet() ? '210deg' : '-70deg'}
+                scale={biggerThanTablet() ? 1 : 0.8}
+                radius={biggerThanTablet() ? 2 : 1.5}
+                cicleTime={8000}
+            />}
+            {/* Background Centro Centro */}
             <AbstractToBlur
-                blur="8rem"
-                opacity={0.25}
-                fill="rgb(0, 255, 194)"
-                bottom={'5rem'}
-                right={'-45rem'}
-                rotate='230deg'
-            />
-            <AbstractToBlur
-                blur="20rem"
-                opacity={mediumDesktop() ? 0.25 : 0.1}
-                fill="rgb(0, 178, 255)"
-                bottom={'-30rem'}
-                right={'10rem'}
-                rotate='210deg'
-            />
-            <AbstractToBlur
-                blur="10rem"
-                opacity={mediumDesktop() ? 1 : 0.1}
-                fill="rgb(16, 16, 16)"
-                bottom={'20%'}
-                right={'25%'}
-                rotate='230deg'
+                blur={biggerThanTablet() ? '10rem' : (biggerThanMobile() && mediumHeight()) ? '20rem' : '5rem'}
+                opacity={mediumDesktop() ? 1 : biggerThanTablet() ? 0.2 : 0.5}
+                fill='background'
+                bottom={(biggerThanMobile() && mediumHeight()) ? '20%' : '-5rem'}
+                right={(biggerThanMobile() && mediumHeight()) ? '25%' : '-10rem'}
+                rotate={(biggerThanMobile() && mediumHeight()) ? '230deg' : '190deg'}
+                radius={(biggerThanMobile() && mediumHeight()) ? 3 : 2}
+                clockWise
             />
         </div>
     )
